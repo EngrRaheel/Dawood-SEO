025e75373666c8913429497b2326839d5beec20d
Engr. Raheel <engr.mraheeltariq@gmail.com>
2023-03-04 22:49:28 +0500

    section below the hero made responsive

--- components/ServiceCard/ServiceCard.jsx
+++ components/ServiceCard/ServiceCard.jsx
@@ -11,11 +11,11 @@ function ServiceCard() {
                     <h2 className='font-extrabold font-Inter text-base text-[#4668c6]'>WEB • EXTENSION • APP</h2>
                     <p className='text-white text-center font-Inter font-extrabold max-w-auto md:max-w-[700px]  text-[28px] md:text-[38px] lg:text-[38px]'>Meet the most advanced growth tools for video creators</p>
                 </div>
-                <div className='w-full grid grid-cols-2 gap-20  justify-center items-start '>
+                <div className='w-full grid  grid-col-1 md:grid-cols-2 gap-20  justify-center items-center md:items-start '>
                     <div className='w-full h-[470px]  relative'>
-                        <Image src={"/Images/cardimage.png"} alt="" fill></Image>
+                        <Image src={"/Images/cardimage.png"} alt="" fill ></Image>
                     </div>
-                    <div className='flex flex-col justify-center items-start w-full text-white gap-6 md:pl-[10%] md:pr-[7%] space-y-6 md:space-y-4 text-center md:text-left'>
+                    <div className='flex flex-col justify-center items-center md:items-start w-full text-white gap-4 md:gap-6 pl-[8%] pr-[4%] md:pl-[10%] md:pr-[7%] space-y-6 md:space-y-4 text-center md:text-left'>
                         <h3 class="font-extrabold text-[24px] md:text-[30px] lg:text-[32px]">Understand what’s working for your channel</h3>
                         <p className='font-Inter text-[#878b8c] font-normal text-[17px] '>Track and compare your performance in real time so you never miss an opportunity to get more views and make money from your videos.</p>
                         <button className='flex justify-center items-center px-6 py-3 rounded-full bg-[#007CEF]'>
